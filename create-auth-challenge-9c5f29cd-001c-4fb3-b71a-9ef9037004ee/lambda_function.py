1e9813ccb82dd0627c28261861ceac7a8dfc48b5
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-16 02:07:19 -0300

    teste-aws-action

--- create-auth-challenge-9c5f29cd-001c-4fb3-b71a-9ef9037004ee/lambda_function.py
+++ create-auth-challenge-9c5f29cd-001c-4fb3-b71a-9ef9037004ee/lambda_function.py
@@ -1,6 +1,6 @@
 import json
 
-def lambda_handler(event, context): 
+def lambda_handler(event, context):
     event['response']['privateChallengeParameters'] = dict()
     event['response']['privateChallengeParameters']['challenge'] = "opensesame"
     return event
